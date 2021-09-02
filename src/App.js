92d21e55a4a8db323cc3fc20e09b41d751214ded
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-09-02 10:44:13 -0400

    Playing with variables/functions and getting their values

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
   
+  foo = () => 'Bars'
   render() {
+    //const name = 'John Doe';
+    //const foo = () => 'Bar'
     return (
-      <div className="App">
-       <h1>Hello from React</h1>
+      <div className='App'>
+       <h1>Hello {this.foo()}</h1>
       </div>
     );
   }

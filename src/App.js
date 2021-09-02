03edc52e5744abca1ba97dd7f97b6e8f0acd3c4c
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-09-02 10:50:54 -0400

    Playing with conditionals (simple)

--- src/App.js
+++ src/App.js
@@ -5,13 +5,22 @@ class App extends Component {
   
   foo = () => 'Bars'
   render() {
-    //const name = 'John Doe';
-    //const foo = () => 'Bar'
-    return (
+    const name = 'John Doe';
+    const loading = false;
+    //if(loading) {
+    //  return <h4>Loading...</h4>
+    //}
+    //return (
+    //  <div className='App'>
+    //   <h1>Hello {name}</h1>
+    //  </div>
+    //);
+    return( 
       <div className='App'>
-       <h1>Hello {this.foo()}</h1>
+          {loading ? <h4>Loading...</h4> : <h1>Hello {name} </h1>}
       </div>
     );
+  
   }
 
 }

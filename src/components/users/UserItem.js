e5e114d7fd5cc70b2f76d92a20f10b1f8e044686
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-09-21 10:39:07 -0400

    Adding UserItem state and picture to UserItem Component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 
-export class UserItem extends Component {
+class UserItem extends Component {
+    constructor() { 
+        super();
+        this.state = {
+            id: 'id',
+            login: 'mojombo',
+            avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+            html_url: "https://github.com/mojombo"
+
+        }
+    }
     render() {
-        return (
-            <div>
-                UserItem
-            </div>
-        )
+        return <div className="card text-center">
+            <img src={this.state.avatar_url}
+            alt=''
+            className='round-img'
+            style={{ width: '60px' }}
+            />
+        </div>
     }
 }
 

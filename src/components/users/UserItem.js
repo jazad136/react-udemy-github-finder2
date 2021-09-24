d6b01c4ca1355eab469a5ba829094e8a33a226ed
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-09-23 21:36:33 -0400

    Removing this.state from all variables

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
-    constructor() { 
-        super();
-        this.state = {
-            id: 'id',
-            login: 'jazad136',
-            avatar_url: 'https://avatars.githubusercontent.com/u/1129384?v=4',
-            html_url: "https://github.com/jazad136"
-
-        }
+    state = {
+        id: 'id',
+        login: 'jazad136',
+        avatar_url: 'https://avatars.githubusercontent.com/u/1129384?v=4',
+        html_url: "https://github.com/jazad136"
     }
+    
     render() {
+        const { login, avatar_url, html_url } = this.state
         return <div className="card text-center">
-            <img src={this.state.avatar_url}
+            <img src={avatar_url}
             alt=''
             className='round-img'
             style={{ width: '60px' }}
             />
-            <h3>{this.state.login}</h3>
+            <h3>{login}</h3>
             <div>
-                <a href={this.state.html_url} className="btn btn-dark btn-sm my-1">
+                <a href={html_url} className="btn btn-dark btn-sm my-1">
                     More
                 </a>
             </div>

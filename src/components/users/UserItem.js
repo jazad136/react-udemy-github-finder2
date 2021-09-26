3e05d5f90949c5e069ebaa930d5c7812c2f64a25
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-09-25 21:35:19 -0400

    Pass static array elements as props. Convert Layout to Grid Style

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
-    state = {
-        id: 'id',
-        login: 'jazad136',
-        avatar_url: 'https://avatars.githubusercontent.com/u/1129384?v=4',
-        html_url: "https://github.com/jazad136"
-    }
     
     render() {
-        const { login, avatar_url, html_url } = this.state
+        const { login, avatar_url, html_url } = this.props.user
         return <div className="card text-center">
-            <img src={avatar_url}
-            alt=''
-            className='round-img'
-            style={{ width: '60px' }}
-            />
+            <img src={avatar_url} 
+            alt='' 
+            className='round-img' 
+            style={{ width: '60px' }}/>
             <h3>{login}</h3>
             <div>
                 <a href={html_url} className="btn btn-dark btn-sm my-1">

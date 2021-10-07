4c83dd1ff47d689f76e3b0404dd6fb3d3eb2af4b
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-10-07 10:28:52 -0400

    Dont use async without await

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 
 class App extends Component {
   
-  async componentDidMount() { 
+  componentDidMount() { 
     axios
     .get('https://api.github.com/users')
     .then(res => console.log(res.data));

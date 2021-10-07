d8e90b8bf33e79da9445137545bffcc1bc7ba214
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-10-07 10:28:15 -0400

    Add github users api call to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import axios from 'axios';
+
 import './App.css';
 
 
 class App extends Component {
   
-  componentDidMount() { 
-    console.log('123')
+  async componentDidMount() { 
+    axios
+    .get('https://api.github.com/users')
+    .then(res => console.log(res.data));
+    
   }
   render() {
     return(<div className="App">

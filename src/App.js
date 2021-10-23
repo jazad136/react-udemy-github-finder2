07d6022fdc0c498068a36343b19ae53f71c34b33
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-10-23 10:18:25 -0400

    Successfully Requesting and Displaying GitHub API Data!

--- src/App.js
+++ src/App.js
@@ -14,13 +14,13 @@ class App extends Component {
   async componentDidMount() { 
     this.setState({loading: true})
     const res = await axios.get('https://api.github.com/users')
-    this.setState({loading: false})
+    this.setState({users: res.data, loading: false})
   }
   render() {
     return(<div className="App">
       <Navbar />
       <div className="container">
-        <Users />
+        <Users loading={this.state.loading} users={this.state.users} />
       </div>
     </div>
     );

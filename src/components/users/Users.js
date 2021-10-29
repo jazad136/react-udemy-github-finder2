54c1e97de4a52398b1f8f2894ab3725fbde1b0b5
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-10-29 11:45:48 -0400

    Spinner gif will show now for long loads

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import UserItem from './UserItem'
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
+
 // export class Users extends Component {
 const Users = ({ users , loading }) => {
-    return (
-        <div style={userStyle}>
-            {users.map(user => (
-                <UserItem key={user.id} user={user}/>
-            ))} 
-        </div>
-    )
+    if(loading) { 
+        return <Spinner />
+    }
+    else { 
+        return (
+            <div style={userStyle}>
+                {users.map(user => (
+                    <UserItem key={user.id} user={user}/>
+                ))} 
+            </div>
+        )
+    }
+}
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
 }
 
 

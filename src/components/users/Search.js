a8bcc428cae44fff337981a38cbead4696e6ce95
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-11-09 18:43:53 -0500

    Added toggleable error message below search bar

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,28 +1,60 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 export class Search extends Component {
     state = {
-        text: ''
+        text: '',
+        textError: ''
+    }
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired
     }
     // text above must match text in the form below
     onSubmit = e => { 
         e.preventDefault();
-        this.props.searchUsers(this.state.text);
-        this.setState({text: ''})
-    }
+        if(this.state.text) { 
+            this.props.searchUsers(this.state.text);
+            this.setState({text: '', textError: ''})
+        }
+        else {    
+            this.setState({text: '', textError: 'Please enter a search string'})
+        }
+    } 
     onChange = (e) => this.setState({ [e.target.name]: e.target.value });
     render() {
-        return (
-            <div>
-                <form onSubmit={this.onSubmit} className="form">
-                    <input type="text" name="text" placeholder="Search users..." 
-                    value={this.state.text}
-                    onChange={this.onChange}/>
-                    <input type="submit" value="search" className="btn btn-dark btn-block"/>
-                </form>
-            </div>
-        )
+        if(!this.state.textError) {
+            return (
+                <div>
+                    <form onSubmit={this.onSubmit} className="form">
+                        <input type="text" name="text" placeholder="Search users..." 
+                        value={this.state.text}
+                        onChange={this.onChange}/>
+                        <input type="submit" value="search" className="btn btn-dark btn-block"/>
+                    </form>
+                    <div>
+                        <h2 style={errorStyle}>&nbsp;</h2>
+                    </div>
+                </div>
+            )
+        }
+        else 
+            return (
+                <div>
+                    <form onSubmit={this.onSubmit} className="form">
+                        <input type="text" name="text" placeholder="Search users..." 
+                        value={this.state.text}
+                        onChange={this.onChange}/>
+                        <input type="submit" value="search" className="btn btn-dark btn-block"/>
+                    </form>
+                    <div>
+                        <h2 style={errorStyle}>{this.state.textError}</h2>
+                    </div>
+                    
+                </div>
+            )
     }
+}
+const errorStyle = {
+    color: 'red'
 }
 
 export default Search

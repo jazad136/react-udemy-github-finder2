35d5d6660a5fc4c764fc239c73aae903a56ad244
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-11-30 19:32:45 -0500

    Create page About.js in Pages folder

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h1>About this App</h1>
+            <p>App to search GitHub users</p>
+            <p>Version 1.0.0</p>
+        </div>
+    )
+}

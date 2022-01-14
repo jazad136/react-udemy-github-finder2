7790eb94da125fb4f1874e0131cbceacb8be6832
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2022-01-13 21:11:58 -0500

    User.js - pull 12 major Github API variables from axios

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
 
 export class User extends Component {
+    
     componentDidMount() { 
-
+        // grabs the login parameter from the url that we used to get here. (wow...) 
+        this.props.getUser(this.props.match.params.login);
     }
 
     render() {
+        const {
+            name, 
+            avatar_url,
+            location,
+            bio,
+            blog,
+            login,
+            html_url,
+            followers,
+            following,
+            public_repos,
+            public_gists,
+            hireable} = this.props.user;
+
+        const{ loading } = this.props;
         return (
             <div>
-                something
+                {name}
             </div>
         )
     }

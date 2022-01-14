0f3a8969daf435cc3dfdd3219dd655fde846f8ae
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2022-01-13 21:17:14 -0500

    Add PropTypes

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react'
-
+import React, { Component, Fragment } from 'react'
+import PropTypes from 'prop-types'
+import Spinner from '../layout/Spinner'
+import {Link} from 'react-router-dom'
 export class User extends Component {
     
     componentDidMount() { 
         // grabs the login parameter from the url that we used to get here. (wow...) 
         this.props.getUser(this.props.match.params.login);
     }
-
+    static propTypes = {
+        loading: PropTypes.bool.isRequired,
+        user: PropTypes.object.isRequired,
+        getUser: PropTypes.object.isRequired
+    }
     render() {
         const {
             name, 
@@ -23,11 +29,12 @@ export class User extends Component {
             hireable} = this.props.user;
 
         const{ loading } = this.props;
-        return (
-            <div>
-                {name}
-            </div>
-        )
+        if(loading) return <Spinner/>
+        return <Fragment>
+            <Link to='/' className='btn btn-light'>
+                Back to Search
+            </Link>
+        </Fragment>
     }
 }
 

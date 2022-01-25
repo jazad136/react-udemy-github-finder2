9e8514a474fec187b26de3e284e387ac7dccf8c0
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-01-25 15:58:11 -0500

    Clean up User.js

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -8,8 +8,9 @@ const User = ({user, loading, getUser, getUserRepos, repos, match}) => {
     useEffect(() => {
       getUser(match.params.login);
       getUserRepos(match.params.login);
+      // eslint-disable-next-line
     }, [])
-
+    
     const {
       name,
       company,

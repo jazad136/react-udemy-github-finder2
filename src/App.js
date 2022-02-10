83b3090f1b79bad5c124c5582d0cbf30ecd6dff5
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-02-10 14:51:43 -0500

    Addr. (#15)(#18) Clean out commented code - Transition from setState to useState

--- src/App.js
+++ src/App.js
@@ -12,14 +12,6 @@ import './App.css';
 import GithubState from './context/github/GithubState';
 
 const App = () => {
-   // state = {
-  //   users: [],
-  //   user: {},
-  //   repos: [],
-  //   loading: false,
-  //   alert: null,
-  //   alertId: null
-  // };
   const [users, setUsers] = useState([]);
   const [user, setUser] = useState({});
   const [repos, setRepos] = useState([]);
@@ -29,7 +21,6 @@ const App = () => {
 
   // Search GitHub Users
   const searchUsers = async (text) => {
-    // this.setState({loading: true});
     setLoading(true);
     const config = {
       [process.env.REACT_APP_GITHUB_CLIENT_ID]:
@@ -40,14 +31,12 @@ const App = () => {
       `https://api.github.com/search/users?q=${text}`,
       config
     );
-    // this.setState({users: res.data.items, loading: false})
     setUsers(res.data.items);
     setLoading(false);
   };
 
   // Get a single GitHub user
   const getUser = async (username) => {
-    // this.setState({loading: true});
     setLoading(true);
     const config = {
       [process.env.REACT_APP_GITHUB_CLIENT_ID]:
@@ -58,14 +47,12 @@ const App = () => {
       `https://api.github.com/users/${username}`,
       config
     );
-    // this.setState({user: res.data, loading: false})
     setLoading(false);
     setUser(res.data);
   };
 
-  // Get users repos
+  // Get user repos
   const getUserRepos = async (username) => {
-    // this.setState({loading: true});
     setLoading(true);
     const config = {
       [process.env.REACT_APP_GITHUB_CLIENT_ID]:
@@ -76,30 +63,25 @@ const App = () => {
       `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc`,
       config
     );
-    // this.setState({repos: res.data, loading: false})
     setRepos(res.data);
     setLoading(false);
   };
+
   // clear users from state
-  // clearUsers = () => this.setState({users: [], loading: false})
   const clearUsers = () => {
     setUsers([]);
     setLoading(false);
   };
+  
   // set an alert
   const showAlert = (msg, type) => {
     setAlert({ msg, type });
-    // this.setState({ alert: { msg: msg, type: type } })
-    // const alId = setTimeout(() => this.setState({ alert: null, alertId : null}), 3000)
-    // this.setState({alertId : alId})
     const alId = setTimeout(() => setAlert(null), 5000);
     setAlertId(alId);
   };
 
   const unsetAlert = () => {
-    // clearTimeout(this.state.alertId)
     clearTimeout(alertId);
-    // this.setState({ alert: null, alertId : null})
     setAlert(null);
     setAlertId(null);
   };

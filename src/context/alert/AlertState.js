264d0c46b1d8997db0548060b396cf88291b2808
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-03-11 17:09:46 -0500

    Cleanup comments

--- src/context/alert/AlertState.js
+++ src/context/alert/AlertState.js
@@ -7,37 +7,25 @@ import {
 } from '../types'
 
 const AlertState = props => {
-    // const initialState = {
-    //     alert: null,
-    //     alertId: null
-    // };
     const initialState = null;
 
     const [state, dispatch] = useReducer(alertReducer, initialState)
 
     // Set Alert
     const setAlert = (msg, type) => {
-        // const alId = setTimeout(() => setAlert(null), 5000);
         dispatch({
             type: SET_ALERT,
             payload: {msg, type}
         });
-        //const alId = setTimeout(() => setAlert(null))
         setTimeout(() => dispatch({ type: REMOVE_ALERT }), 5000);
-        
-        // setAlert({ msg, type });
-        // setAlertId(alId);
     };
+    
+    // Unset Alert
     const unsetAlert = () => {
         dispatch({ type: REMOVE_ALERT })
-        // clearTimeout(alertId);
-        // setAlert(null);
-        // setAlertId(null);
     };
     return (<AlertContext.Provider
         value = {{
-            // alert: state.alert,
-            // alertId: state.alertId,
             alert: state,
             setAlert,
             unsetAlert
